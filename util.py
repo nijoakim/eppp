d011c7d724e4aab18619c105995c2c02d1048d11
Joakim Nilsson <nijoakim@gmail.com>
2015-05-17 21:02:58 +0200

    Further improvements

--- util.py
+++ util.py
@@ -58,24 +58,43 @@ def _seriesRes(res1, res2):
 	res2 = float(res2)
 	return res1 + res2
 
-# TODO: Custom series
-# TODO: Loop for smaller values also
 def _getAvailVals(series = 'E6', minVal = 10, maxVal = 10000000):
 	# Dictionry with common series
+	# TODO: Add more
+	# TODO: Calculate these?
 	availValsDict = {}
 	availValsDict['E6']  = [10.,      15.,      22.,      33.,      47.,      68.     ]
 	availValsDict['E12'] = [10., 12., 15., 18., 22., 27., 33., 39., 47., 56., 68., 82.]
 	
-	# Generate higher order series
-	basicAvailVals = availValsDict[series]
+	# Get basic available values
+	if type(series) is str:
+		basicAvailVals = availValsDict[series]
+	else:
+		basicAvailVals = series
 	availVals = list(basicAvailVals)
+	
+	# Append higher order series
 	multiplier = 10
 	while availVals[-1] <= maxVal:
-		availVals += map(lambda x: x*multiplier, basicAvailVals)
+		availVals  += map(lambda x: x*multiplier, basicAvailVals)
 		multiplier *= 10
+	
+	# Reverse for more efficient list operations
+	availVals.reverse()
+	basicAvailVals.reverse()
+	
+	# Append low order series
+	divider = 10
+	while availVals[-1] >= minVal:
+		availVals += map(lambda x: x / divider, basicAvailVals)
+		divider   *= 10
+	
+	# Reverse again for ordered list
+	availVals.reverse()
+	
+	# Filter out too high or too low values
 	availVals = filter(lambda x: x <= maxVal, availVals)
 	availVals = filter(lambda x: x >= minVal, availVals)
-	availVals += [0]
 	
 	return availVals
 
@@ -85,59 +104,62 @@ def findResistorNetwork(target, availVals = _getAvailVals('E6'), availOps = [_pa
 	expr = None
 	while abs(relError) > maxRelError:
 		print('Trying with %i components...' % numComps) # TODO: Log with log.py (level 1)
-		expr  = bestResistorNetwork(target, numComps = numComps, availVals = availVals, availOps = availOps)
+		expr  = bestResistorNetwork(target, maxNumComps = numComps, availVals = availVals, availOps = availOps, recurse = False)
 		value = _polishEval(expr)[0]
 		relError = (target - value) / target
-		print('Best relative error so far: %f %%' % (100*relError)) # TODO: Log with log.py (level 2) $ TODO: standard significant digits
+		print('Best relative error so far: %f %%' % (100*relError)) # TODO: Log with log.py (level 2) $ TODO: Print with standard significant digits
 		numComps += 1
 	return expr
-		
 
-def bestResistorNetwork(target, numComps, availVals = _getAvailVals('E6'), availOps = [_parallelRes, _seriesRes]):
+def bestResistorNetwork(target, maxNumComps, availVals = _getAvailVals('E6'), availOps = [_parallelRes, _seriesRes], recurse = True):
 	bestError = float('inf')
 	bestExpr  = None
 	
+	# Base case
+	if maxNumComps == 0:
+		return None
+	
+	# Find expressions of lower number of components if recursion is enabled
+	elif recurse:
+		bestExpr = bestResistorNetwork(target, maxNumComps - 1, availVals, availOps)
+		
+		# Update best error
+		if not bestExpr is None:
+			bestError = abs(target - _polishEval(bestExpr)[0])
+	
 	# For every combination
-	for values in _it.combinations_with_replacement(availVals, numComps):
-		for ops in _it.product(availOps, repeat = numComps - 1):
-			for insertions in _it.product([0, 1], repeat = numComps - 1):
+	for values in _it.combinations_with_replacement(availVals, maxNumComps):
+		for ops in _it.product(availOps, repeat = maxNumComps - 1):
+			for insertions in _it.product([0, 1], repeat = maxNumComps - 1):
+				# Initilize expression and insert position
 				expr = list(values)
 				insertPos = 0
 				
+				# TODO: This method produces redundant expressions... Can it be improved?
+				# Insert functions in expression in every valid way
 				for insertion in insertions:
 					opList = list(ops)
 					expr.insert(insertPos, opList.pop())
 					insertPos += insertion + 1
 				
-				try:
-					evald = _polishEval(expr) # Evaluate expression polishly
-					
-					# Check that expression is completely evaluated
-					if len(evald) != 1:
-						raise Exception('Expression is not completely evaluated.')
-					
-					value = evald[0]            # Get value from evaluated expression
-					error = abs(target - value) # Calculate error
-					
-					# Remember result if best so far
-					if error < bestError:
-						bestError = error
-						bestCombination = expr
+				value = _polishEval(expr)[0] # Get value from evaluated expression
+				error = abs(target - value)  # Calculate error
 				
-				# Throw away all exceptions
-				except:
-					pass
+				# Remember result if best so far
+				if error <= bestError:
+					bestError = error
+					bestExpr = expr
 	
 	# TODO: Logging
 	
-	return bestCombination
+	return bestExpr
 
 # TODO: Remove
 # Test
 import time
 t = time.time()
-bc = bestResistorNetwork(860000, 6, availVals = _getAvailVals('E6', minVal = 100000))
-# bc = findResistorNetwork(88123, availVals = _getAvailVals('E6', minVal = 100000), maxRelError = 0.0003)
+# bc = bestResistorNetwork(13, 3, availVals = _getAvailVals('E6', minVal = 10))
+bc = findResistorNetwork(88123, availVals = _getAvailVals('E6', minVal = 10), maxRelError = 0.001005)
 _dbg.printVar(time.time() - t, 'time')
 print bc
 print _polishEval(bc)
