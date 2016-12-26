ccb86075acb8140b4157d917425e8ca40d79f361
Joakim Nilsson <nijoakim@gmail.com>
2016-12-26 11:52:53 +0100

    More unit tests for 'lumped_network'.

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -36,11 +36,29 @@ eppp.set_log_level(0)
 #============
 
 class TestStringMethods(ut.TestCase):
-	# TODO: Finish this
 	def test_lumped_network(self):
+		# 1 component
 		self.assertEqual(
-			str(eppp.calc.lumped_network(88120, max_num_comps = 3)),
+			str(eppp.calc.lumped_network(88120, max_num_comps=1)),
+			'100.0 k'
+		)
+
+		# 2 components
+		self.assertEqual(
+			str(eppp.calc.lumped_network(88120, max_num_comps=2)),
+			'(100.0 k || 680.0 k)'
+		)
+
+		# 3 components
+		self.assertEqual(
+			str(eppp.calc.lumped_network(88120, max_num_comps=3)),
 			'(220.0 k || (47.00 k + 100.0 k))'
+		)
+
+		# 3 components, solved by 2
+		self.assertEqual(
+			str(eppp.calc.lumped_network(16800, max_num_comps=3, max_rel_error=0)),
+			'(6.800 k + 10.00 k)'
 		)
 
 	def test_parallel_imp(self):
