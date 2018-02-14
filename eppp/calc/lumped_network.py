1675fa7cafd0a16a90b94e0dbc5cd2dbaba25ec5
Joakim Nilsson <nijoakim@gmail.com>
2018-02-14 23:04:58 +0100

    lumped_network: Added Jimmy's sausage code. Still needs fixing up.

--- eppp/calc/lumped_network.py
+++ eppp/calc/lumped_network.py
@@ -1,5 +1,5 @@
-# Copyright 2015-2017 Joakim Nilsson
-# Copyright 2016      Jimmy Nyström
+# Copyright 2015-2018 Joakim Nilsson
+# Copyright 2016-2017 Jimmy Nyström
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -24,7 +24,8 @@ import functools as _ft
 import itertools as _it
 import operator  as _op
 import numpy     as _np
-from math import inf
+from bisect import bisect
+from math   import inf
 
 # Internal
 from ..debug import *
@@ -449,216 +450,301 @@ except ImportError:
 def lumped_network(
 		target,
 		max_num_comps = inf,
-		max_rel_error = None,
+		max_rel_error = None, # TODO: Use
 		max_abs_error = None,
 		avail_vals    = get_avail_vals('E6'),
-		avail_ops     = [parallel_impedance, _add],
+		avail_ops     = [parallel_impedance, _add], # TODO: Use
+		pre_calc_depth = 0,
 	):
-	"""
-	Finds a network of passive components matching a specified (possibly complex) value.
+	pre_calc = {}
+	for val in avail_vals:
+		pre_calc[val] = [val]
+	for i in range(1, pre_calc_depth):
+		next_pre_calc = pre_calculate_exprs(avail_vals, pre_calc)
+		next_pre_calc.update(pre_calc)
+		pre_calc = next_pre_calc
+	pre_calc_values = sorted(pre_calc.keys())
+	max_abs_error = 0.001 # TODO: Do it properly
+	for i in range(max_num_comps):
+		res = lumpy_helper(avail_vals, i, target, pre_calc, pre_calc_values, pre_calc_depth, max_abs_error)
+		if abs(res[-1] - target) < max_abs_error:
+			break
+	# print(res)
+	return res
 
-	Args:
-		target (number):        Target impedance for the network.
+PARALLEL  = '||'
+SERIES    = '+'
 
-	Kwargs:
-		max_num_comps (int):    Maximum number of components in the network. The function will return a network with no more components than this value.
-		max_rel_error (number): (Default 0.01) Maximum tolerable relative error. The relative error of the resulting network will be less than this value if that is possible given 'max_num_comps'.
-		max_abs_error (number): Maximum tolerable absolute error. The absolute error of the resulting network will be less than this value if that is possible given 'max_num_comps'.
-		avail_vals ([number]):  List of available values of the impedances used in the network.
-		avail_ops ([operator]): (Default: [eppp.calc.parallel_impedance, operator.add]) List of operators used for calculating the resulting impedance. The operators are represented by a function that takes to arguments. The operators must be associative and commutative.
-
-	Returns:
-		ExprTree. Expression tree of the resulting network. Use ExprTree.evaluate() to get it's value.
-	"""
-
-	# Make target real type if it is real
-	if complex(target).imag == 0:
-		target = complex(target).real
-
-	# Check what types of numbers 'avail_vals' contains
-	has_complex_vals = complex in map(type, avail_vals)
-	if has_complex_vals:
-		may_have_negative_vals = True
-	else:
-		may_have_negative_vals = True in map(lambda x: x < 0, avail_vals)
-
-	# Sort 'avail_vals' if not complex
-	if not has_complex_vals:
-		avail_vals.sort()
-
-	# Use more general parallel function if 'avail_vals' contains 0, infinity or negative values
-	if 0 in avail_vals \
-	or inf in avail_vals \
-	or may_have_negative_vals:
-		avail_ops = list(map(lambda x: parallel_impedance if x == _parallel_impedance_non_strict else x, avail_ops))
-
-	# Use less general function otherwise
-	else:
-		avail_ops = list(map(lambda x: _parallel_impedance_non_strict if x == parallel_impedance else x, avail_ops))
-
-	# Determine whether a strict polish_eval function must be used
-	polish_eval_func = _polish_eval_non_strict
-	for op in avail_ops:
-		if  op != _add \
-		and op != _parallel_impedance_non_strict:
-			polish_eval_func = lambda x: _polish_eval(x)[0]
-
-	# Don't display a unit (may change later)
-	# unit = 'Ω'
-	unit = ''
-
-	# Check so that target is non-zero
-	if target == 0:
-		raise ValueError("Target impedance must be non-zero.")
-
-	# Figure out error mode
-	if max_abs_error is None and max_rel_error is None:
-		max_error = 0.01
-		use_rel_error = True
-	elif not max_abs_error is None and not max_rel_error is None:
-		raise ValueError("Can not specify both 'max_abs_error' and 'max_rel_error'.")
-	elif not max_abs_error is None:
-		max_error = max_abs_error
-		use_rel_error = False
-	else:
-		max_error = max_rel_error
-		use_rel_error = True
-
-	# Initial values
+def lumpy_helper(avail_vals, num_comps, target, pre_calc, pre_calc_values, pre_calc_depth, max_error):
 	best_error = inf
-	best_value = None
+	best_val = None
 	best_expr  = None
 
-	# Insertions generator
-	def insertions_gen(ops, last_op = None):
-		# Base case
-		if len(ops) == 0:
-			yield [-1]
+	index = bisect(pre_calc_values, target)
+	if index < len(pre_calc):
+		best_val = pre_calc_values[index]
+		best_expr = pre_calc[best_val]
+		best_error = abs(target - best_val)
+	if index > 0:
+		val = pre_calc_values[index - 1]
+		expr = pre_calc[val]
+		if abs(target - val) < best_error:
+			best_val = val
+			best_expr = expr
+			best_error = abs(target - best_val)
 
-		# Recurse
-		else:
-			cur_op = ops[0]
-			for insertions in insertions_gen(ops[1:], last_op = cur_op):
-				# Avoid redundant insertion variations by not varying operator order in case of consecutive equal operations
-				if cur_op == last_op or cur_op == None:
-					yield [2] + insertions
+	if num_comps <= pre_calc_depth or best_error <= max_error:
+		return (best_expr, best_val)
 
-				# Non-redundant case
-				else:
-					yield [1] + insertions
-					yield [2] + insertions
+	for val in avail_vals:
+		if val == target:
+			return ([val], val)
+		if val < target:
+			series_rest = target - val
+			series_expr, series_val = lumpy_helper(avail_vals, num_comps - 1, series_rest, pre_calc, pre_calc_values, pre_calc_depth, max_error)
+			series_error = abs(series_rest - series_val)
+			if series_error < best_error:
+				best_error = series_error
+				best_val = series_val + val
+				best_expr = [SERIES, val, *series_expr]
+				if best_error <= max_error:
+					return (best_expr, best_val)
+		if target < val:
+			parallel_rest = (val * target) / (val - target)
+			parallel_expr, parallel_val = lumpy_helper(avail_vals, num_comps - 1, parallel_rest, pre_calc, pre_calc_values, pre_calc_depth, max_error)
+			parallel_error = abs(parallel_rest - parallel_val)
+			if parallel_error < best_error:
+				best_error = parallel_error
+				best_val = (val * parallel_val) / (val + parallel_val)
+				best_expr = [PARALLEL, val, *parallel_expr]
+				if best_error <= max_error:
+					return (best_expr, best_val)
+	return (best_expr, best_val)
 
-	# For all numbers of components, starting from one and up until return
-	for num_comps in _it.count(1):
-		_log(1, 'Finding lumped network for %i component%s...' % (num_comps, '' if num_comps == 1 else 's'))
+def pre_calculate_exprs(vals, pre_calc):
+	result = {}
+	for val in vals:
+		for pre_calc_val in pre_calc:
+			expr = pre_calc[pre_calc_val]
+			series_val = val + pre_calc_val
+			result[series_val] = [SERIES, val, *expr]
+			parallel_val = (val * pre_calc_val) / (val + pre_calc_val)
+			result[parallel_val] = [PARALLEL, val, *expr]
+	return result
 
-		# Initialize stuff that can be known in advance for performance
-		num_ops  = num_comps - 1
-		expr_len = 2 * num_comps - 1
-		expr     = [0] * expr_len
+# def lumped_network(
+# 		target,
+# 		max_num_comps = inf,
+# 		max_rel_error = None,
+# 		max_abs_error = None,
+# 		avail_vals    = get_avail_vals('E6'),
+# 		avail_ops     = [parallel_impedance, _add],
+# 	):
+# 	"""
+# 	Finds a network of passive components matching a specified (possibly complex) value.
 
-		# Precalculate lists of non-redundant combinations of operations
-		ops_len             = 2 ** num_ops
-		ops_range           = range(ops_len)
-		insertions_pre_calc = [0] * ops_len
-		ops_pre_calc        = [0] * ops_len
-		index               = 0
-		for ops in _it.product(avail_ops, repeat = num_ops):
-			ops_pre_calc[index] = ops
-			if num_comps == 1:
-				insertions_pre_calc[index] = [[-1]]
-			elif num_comps <= 2:
-				insertions_pre_calc[index] = [[0, -1]]
-			else:
-				insertions_pre_calc[index] = []
-				for insertions in insertions_gen(ops[1:], ops[0]):
-					insertions_pre_calc[index].append([0] + insertions)
-			index += 1
+# 	Args:
+# 		target (number):        Target impedance for the network.
 
-		# For all non-redundant combinations of values
-		for values in _it.combinations_with_replacement(avail_vals, num_comps):
-			# Do not calculate obvious sub-optimal values
-			if not may_have_negative_vals:
-				if sum(values) < target - best_error:
-					continue
-				# if parallel_impedance(*values) > target + best_error:
-				# 	continue
-				# Approximation (subset) of the above which is faster to calculate
-				if values[0] > (target + best_error) * num_comps: # 'values[0]' = 'min(values)' since list is sorted
-					continue
+# 	Kwargs:
+# 		max_num_comps (int):    Maximum number of components in the network. The function will return a network with no more components than this value.
+# 		max_rel_error (number): (Default 0.01) Maximum tolerable relative error. The relative error of the resulting network will be less than this value if that is possible given 'max_num_comps'.
+# 		max_abs_error (number): Maximum tolerable absolute error. The absolute error of the resulting network will be less than this value if that is possible given 'max_num_comps'.
+# 		avail_vals ([number]):  List of available values of the impedances used in the network.
+# 		avail_ops ([operator]): (Default: [eppp.calc.parallel_impedance, operator.add]) List of operators used for calculating the resulting impedance. The operators are represented by a function that takes to arguments. The operators must be associative and commutative.
 
-			# For all non-redundant combinations of operations
-			for op_index in ops_range:
-				# Use pre-calculated list of operations
-				ops = ops_pre_calc[op_index]
+# 	Returns:
+# 		ExprTree. Expression tree of the resulting network. Use ExprTree.evaluate() to get it's value.
+# 	"""
 
-				# Build expression from insertions
-				for insertions in insertions_pre_calc[op_index]:
-					if num_comps == 1:
-						value   = values[0]
-						expr[0] = value
-					else:
-						expr[0]   = ops[0]
-						op_index  = 1
-						val_index = 0
-						index     = 1
-						while op_index < num_ops:
-							if insertions[op_index] == 2:
-								expr[index] = values[val_index]
-								val_index += 1
-								index     += 1
-							expr[index] = ops[op_index]
-							op_index += 1
-							index    += 1
-						expr[index:] = values[val_index:]
+# 	# Make target real type if it is real
+# 	if complex(target).imag == 0:
+# 		target = complex(target).real
 
-						# Get value from evaluated expression
-						value = polish_eval_func(expr)
+# 	# Check what types of numbers 'avail_vals' contains
+# 	has_complex_vals = complex in map(type, avail_vals)
+# 	if has_complex_vals:
+# 		may_have_negative_vals = True
+# 	else:
+# 		may_have_negative_vals = True in map(lambda x: x < 0, avail_vals)
 
-					# Calculate error
-					error = abs(value - target)
+# 	# Sort 'avail_vals' if not complex
+# 	if not has_complex_vals:
+# 		avail_vals.sort()
 
-					# Remember result if best so far
-					if error <= best_error:
-						# Remember best things
-						best_error = error
-						best_value = value
+# 	# Use more general parallel function if 'avail_vals' contains 0, infinity or negative values
+# 	if 0 in avail_vals \
+# 	or inf in avail_vals \
+# 	or may_have_negative_vals:
+# 		avail_ops = list(map(lambda x: parallel_impedance if x == _parallel_impedance_non_strict else x, avail_ops))
 
-						# Rebuild the expression
-						best_expr  = [0] * expr_len
-						insert_pos = insertions[0]
-						op_index   = 0
-						val_index  = 0
-						index      = 0
-						while index < expr_len:
-							if index == insert_pos:
-								best_expr[index] = ops[op_index]
-								op_index   += 1
-								insert_pos += insertions[op_index]
-							else:
-								best_expr[index] = values[val_index]
-								val_index += 1
-							index += 1
+# 	# Use less general function otherwise
+# 	else:
+# 		avail_ops = list(map(lambda x: _parallel_impedance_non_strict if x == parallel_impedance else x, avail_ops))
 
-						# Return if an optimal solution has been found
-						if best_error == 0:
-							return ExprTree(best_expr, unit = unit)
+# 	# Determine whether a strict polish_eval function must be used
+# 	polish_eval_func = _polish_eval_non_strict
+# 	for op in avail_ops:
+# 		if  op != _add \
+# 		and op != _parallel_impedance_non_strict:
+# 			polish_eval_func = lambda x: _polish_eval(x)[0]
 
-		# Calculate error
-		if use_rel_error:
-			best_signed_error = (best_value - target) / abs(target) # Division-by-zero safe since target can't be 0
-		else:
-			best_signed_error = best_value - target
+# 	# Don't display a unit (may change later)
+# 	# unit = 'Ω'
+# 	unit = ''
 
-		# Log best error so far
-		if use_rel_error:
-			_log(2, 'Best relative error so far: %s' % (_str_sci(best_signed_error)))
-		else:
-			_log(2, 'Best absolute error so far: %s' % (_str_sci(best_signed_error)))
+# 	# Check so that target is non-zero
+# 	if target == 0:
+# 		raise ValueError("Target impedance must be non-zero.")
 
-		# Log best network
-		_log(3, 'Best network so far: '+ str(ExprTree(best_expr, unit = unit)))
+# 	# Figure out error mode
+# 	if max_abs_error is None and max_rel_error is None:
+# 		max_error = 0.01
+# 		use_rel_error = True
+# 	elif not max_abs_error is None and not max_rel_error is None:
+# 		raise ValueError("Can not specify both 'max_abs_error' and 'max_rel_error'.")
+# 	elif not max_abs_error is None:
+# 		max_error = max_abs_error
+# 		use_rel_error = False
+# 	else:
+# 		max_error = max_rel_error
+# 		use_rel_error = True
 
-		# Return if sufficiently good or maximum number of components have been used
-		if abs(best_signed_error) <= max_error or num_comps == max_num_comps:
-			return ExprTree(best_expr, unit = unit)
+# 	# Initial values
+# 	best_error = inf
+# 	best_value = None
+# 	best_expr  = None
+
+# 	# Insertions generator
+# 	def insertions_gen(ops, last_op = None):
+# 		# Base case
+# 		if len(ops) == 0:
+# 			yield [-1]
+
+# 		# Recurse
+# 		else:
+# 			cur_op = ops[0]
+# 			for insertions in insertions_gen(ops[1:], last_op = cur_op):
+# 				# Avoid redundant insertion variations by not varying operator order in case of consecutive equal operations
+# 				if cur_op == last_op or cur_op == None:
+# 					yield [2] + insertions
+
+# 				# Non-redundant case
+# 				else:
+# 					yield [1] + insertions
+# 					yield [2] + insertions
+
+# 	# For all numbers of components, starting from one and up until return
+# 	for num_comps in _it.count(1):
+# 		_log(1, 'Finding lumped network for %i component%s...' % (num_comps, '' if num_comps == 1 else 's'))
+
+# 		# Initialize stuff that can be known in advance for performance
+# 		num_ops  = num_comps - 1
+# 		expr_len = 2 * num_comps - 1
+# 		expr     = [0] * expr_len
+
+# 		# Precalculate lists of non-redundant combinations of operations
+# 		ops_len             = 2 ** num_ops
+# 		ops_range           = range(ops_len)
+# 		insertions_pre_calc = [0] * ops_len
+# 		ops_pre_calc        = [0] * ops_len
+# 		index               = 0
+# 		for ops in _it.product(avail_ops, repeat = num_ops):
+# 			ops_pre_calc[index] = ops
+# 			if num_comps == 1:
+# 				insertions_pre_calc[index] = [[-1]]
+# 			elif num_comps <= 2:
+# 				insertions_pre_calc[index] = [[0, -1]]
+# 			else:
+# 				insertions_pre_calc[index] = []
+# 				for insertions in insertions_gen(ops[1:], ops[0]):
+# 					insertions_pre_calc[index].append([0] + insertions)
+# 			index += 1
+
+# 		# For all non-redundant combinations of values
+# 		for values in _it.combinations_with_replacement(avail_vals, num_comps):
+# 			# Do not calculate obvious sub-optimal values
+# 			if not may_have_negative_vals:
+# 				if sum(values) < target - best_error:
+# 					continue
+# 				# if parallel_impedance(*values) > target + best_error:
+# 				# 	continue
+# 				# Approximation (subset) of the above which is faster to calculate
+# 				if values[0] > (target + best_error) * num_comps: # 'values[0]' = 'min(values)' since list is sorted
+# 					continue
+
+# 			# For all non-redundant combinations of operations
+# 			for op_index in ops_range:
+# 				# Use pre-calculated list of operations
+# 				ops = ops_pre_calc[op_index]
+
+# 				# Build expression from insertions
+# 				for insertions in insertions_pre_calc[op_index]:
+# 					if num_comps == 1:
+# 						value   = values[0]
+# 						expr[0] = value
+# 					else:
+# 						expr[0]   = ops[0]
+# 						op_index  = 1
+# 						val_index = 0
+# 						index     = 1
+# 						while op_index < num_ops:
+# 							if insertions[op_index] == 2:
+# 								expr[index] = values[val_index]
+# 								val_index += 1
+# 								index     += 1
+# 							expr[index] = ops[op_index]
+# 							op_index += 1
+# 							index    += 1
+# 						expr[index:] = values[val_index:]
+
+# 						# Get value from evaluated expression
+# 						value = polish_eval_func(expr)
+
+# 					# Calculate error
+# 					error = abs(value - target)
+
+# 					# Remember result if best so far
+# 					if error <= best_error:
+# 						# Remember best things
+# 						best_error = error
+# 						best_value = value
+
+# 						# Rebuild the expression
+# 						best_expr  = [0] * expr_len
+# 						insert_pos = insertions[0]
+# 						op_index   = 0
+# 						val_index  = 0
+# 						index      = 0
+# 						while index < expr_len:
+# 							if index == insert_pos:
+# 								best_expr[index] = ops[op_index]
+# 								op_index   += 1
+# 								insert_pos += insertions[op_index]
+# 							else:
+# 								best_expr[index] = values[val_index]
+# 								val_index += 1
+# 							index += 1
+
+# 						# Return if an optimal solution has been found
+# 						if best_error == 0:
+# 							return ExprTree(best_expr, unit = unit)
+
+# 		# Calculate error
+# 		if use_rel_error:
+# 			best_signed_error = (best_value - target) / abs(target) # Division-by-zero safe since target can't be 0
+# 		else:
+# 			best_signed_error = best_value - target
+
+# 		# Log best error so far
+# 		if use_rel_error:
+# 			_log(2, 'Best relative error so far: %s' % (_str_sci(best_signed_error)))
+# 		else:
+# 			_log(2, 'Best absolute error so far: %s' % (_str_sci(best_signed_error)))
+
+# 		# Log best network
+# 		_log(3, 'Best network so far: '+ str(ExprTree(best_expr, unit = unit)))
+
+# 		# Return if sufficiently good or maximum number of components have been used
+# 		if abs(best_signed_error) <= max_error or num_comps == max_num_comps:
+# 			return ExprTree(best_expr, unit = unit)
